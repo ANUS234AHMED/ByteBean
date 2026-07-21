e45d0c696695c912c23403242d5c331242577804
Anus Ahmed <manus.spsc2.6560@gmail.com>
2026-07-21 23:18:23 +0500

    implenting professional recipt

--- main.py
+++ main.py
@@ -90,5 +90,26 @@ while True:
     if again != "yes":
         break
 
+#for discount calc
+
+if sum(order['total'] for order in cart) > 1000:
+    print("\nCongratulations! You have received a 10% discount on your total bill.")
+elif sum(order['total'] for order in cart) > 500:
+    print("\nCongratulations! You have received a 5% discount on your total bill.")
+else :
+    print("\nDiscounts are on order above Rs.500 only. No discount applied.")
+
+
+# for final professional recipt :
+print("\n======= BYTEBEAN RECEIPT =======")
+print("customer name: ", customer_name)
 for order in cart:
-    print(f"{order['quantity']} x {order['name']} ({order['category']}) - Rs.{order['total']}")
+    print(f"\n({order['category']}) \n{order['name']} x {order['quantity']} \nRs.{order['total']}")
+    print("-----------------------------")
+
+print("\nTotal Amount: Rs.", sum(order['total'] for order in cart))
+print("discount applied: Rs.", sum(order['total'] for order in cart) * 0.1 if sum(order['total'] for order in cart) > 1000 else sum(order['total'] for order in cart) * 0.05 if sum(order['total'] for order in cart) > 500 else 0)
+print("Final Amount: Rs.", sum(order['total'] for order in cart) - (sum(order['total'] for order in cart) * 0.1 if sum(order['total'] for order in cart) > 1000 else sum(order['total'] for order in cart) * 0.05 if sum(order['total'] for order in cart) > 500 else 0))
+print("Thank you for visiting ByteBean! Have a great day!")
+    
+    
