1b29e7db069a9e33c339737604a1183bd33fbe30
Anus Ahmed <manus.spsc2.6560@gmail.com>
2026-07-22 22:41:34 +0500

    refactor:organizing by using function

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-print("====== Welcome to ByteBean ======")
-
-customer_name = input("Please enter your name: ")
-
 drink_menu = {
     "1": {
         "category": "Coffee",
@@ -44,40 +40,100 @@ drink_menu = {
     }
 }
 
-# Create the cart ONCE
-cart = []
 
-while True:
 
+def show_menu(drink_menu):
     print("\n======= BYTEBEAN MENU =======")
 
     for key, value in drink_menu.items():
         print(f"{key}. {value['category']}")
 
-    category = input("\nSelect a category (1-4): ")
-
-    if category not in drink_menu:
-        print("Invalid category!")
-        exit()
-
+def show_drinks(drink_menu,category):
+    
     print(f"\n----- {drink_menu[category]['category']} Menu -----")
 
     for key, value in drink_menu[category]["items"].items():
         print(f"{key}. {value['name']} - Rs.{value['price']}")
 
-    drink = input("\nSelect your drink: ")
+def print_receipt(customer_name,cart,grand_total, discount_percent, discount, final_amount):
+    print("\n====================================")
+    print("        BYTEBEAN RECEIPT")
+    print("====================================")
 
-    if drink not in drink_menu[category]["items"]:
+    print(f"Customer Name : {customer_name}")
+
+    for order in cart:
+       print("------------------------------------")
+       print(f"Category : {order['category']}")
+       print(f"Drink    : {order['name']}")
+       print(f"Price    : Rs.{order['price']}")
+       print(f"Quantity : {order['quantity']}")
+       print(f"Total    : Rs.{order['total']}")
+
+
+    print("------------------------------------")
+    print(f"Grand Total      : Rs.{grand_total}")
+    print(f"Discount ({discount_percent}%) : Rs.{discount:.2f}")
+    print(f"Final Amount     : Rs.{final_amount:.2f}")
+    print("====================================")
+    print("Thank you for visiting ByteBean ☕")
+
+def calculate_discount(cart):
+    
+    
+    grand_total = sum(order["total"] for order in cart)
+    discount = 0
+    discount_percent = 0
+
+    if grand_total >= 1000:
+       discount_percent = 10
+       discount = grand_total * 0.10
+       print("\nCongratulations! You received a 10% discount!")
+
+    elif grand_total >= 500:
+        discount_percent = 5
+        discount = grand_total * 0.05
+        print("\nCongratulations! You received a 5% discount!")
+
+    else:
+         print("\nNo discount applied. Orders above Rs.500 get a discount.")
+
+    final_amount = grand_total - discount
+
+    return grand_total, discount_percent, discount, final_amount
+
+def main():
+
+    print("====== Welcome to ByteBean ======")
+    
+    customer_name = input("Please enter your name: ")
+
+    cart = []
+
+    while True:
+
+      show_menu(drink_menu)   
+      category = input("\nSelect a category (1-4): ")
+
+      if category not in drink_menu:
+        print("Invalid category!")
+        exit()
+
+      show_drinks(drink_menu, category)
+
+      drink = input("\nSelect your drink: ")
+
+      if drink not in drink_menu[category]["items"]:
         print("Invalid drink!")
         exit()
 
-    num_of_cups = int(input("How many cups would you like to order? "))
+      num_of_cups = int(input("How many cups would you like to order? "))
 
-    selected_item = drink_menu[category]["items"][drink]
+      selected_item = drink_menu[category]["items"][drink]
 
-    total_amount = selected_item["price"] * num_of_cups
+      total_amount = selected_item["price"] * num_of_cups
 
-    cart.append({
+      cart.append({
     "category": drink_menu[category]["category"],
     "name": selected_item["name"],
     "price": selected_item["price"],
@@ -85,31 +141,16 @@ while True:
     "total": total_amount
 })
 
-    again = input("Would you like to order another drink? (yes/no): ").lower()
+      again = input("Would you like to order another drink? (yes/no): ").lower()
 
-    if again != "yes":
-        break
+      if again != "yes":
+       break
 
-#for discount calc
+    grand_total, discount_percent, discount, final_amount = calculate_discount(cart)
 
-if sum(order['total'] for order in cart) > 1000:
-    print("\nCongratulations! You have received a 10% discount on your total bill.")
-elif sum(order['total'] for order in cart) > 500:
-    print("\nCongratulations! You have received a 5% discount on your total bill.")
-else :
-    print("\nDiscounts are on order above Rs.500 only. No discount applied.")
+    print_receipt(customer_name, cart, grand_total, discount_percent, discount, final_amount)
 
+    print("Please visit again!")
+    print("====================================")
 
-# for final professional recipt :
-print("\n======= BYTEBEAN RECEIPT =======")
-print("customer name: ", customer_name)
-for order in cart:
-    print(f"\n({order['category']}) \n{order['name']} x {order['quantity']} \nRs.{order['total']}")
-    print("-----------------------------")
-
-print("\nTotal Amount: Rs.", sum(order['total'] for order in cart))
-print("discount applied: Rs.", sum(order['total'] for order in cart) * 0.1 if sum(order['total'] for order in cart) > 1000 else sum(order['total'] for order in cart) * 0.05 if sum(order['total'] for order in cart) > 500 else 0)
-print("Final Amount: Rs.", sum(order['total'] for order in cart) - (sum(order['total'] for order in cart) * 0.1 if sum(order['total'] for order in cart) > 1000 else sum(order['total'] for order in cart) * 0.05 if sum(order['total'] for order in cart) > 500 else 0))
-print("Thank you for visiting ByteBean! Have a great day!")
-    
-    
+main()
